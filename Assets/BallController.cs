8287ea7b562a7839114e01f44ec91b8782172e43
thndlovu <tndlovu@student.ubc.ca>
2025-01-28 21:01:36 -0800

    Updated ballSpeed initialization.
    Corrected a mistake in the ballSpeed variable's initialization.

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour {
     public Rigidbody sphereRigidbody;
-    public ballSpeed = 2f;
+    public float ballSpeed = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
         
